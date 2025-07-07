78ef30dec403ff0b3f0a6435674ae8e86cb5d3f8
Brian Meinert <brian.meinert1@gmail.com>
2025-07-07 12:40:05 -0400

    added functionality to the nav buttons displaying proper sections

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 const contactButton = document.querySelector('.js-contact-btn');
+const navButtons = document.querySelectorAll('.js-nav-button');
+const sections = {
+  about: document.querySelector ('.js-about'),
+  projects: document.querySelector('.js-projects'),
+  blog: document.querySelector('.js-blog')
+};
+
+// event listener for contact button
 
 contactButton.addEventListener('click', () => {
   const contactDisplay = document.querySelector('.js-contact-info');
@@ -9,4 +17,26 @@ contactButton.addEventListener('click', () => {
       contactButton.innerText = `Show Contact`;
     }
   }, 275);
+});
+
+// event listener for nav buttons
+
+navButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const targetSection = button.getAttribute('data-section');
+
+    navButtons.forEach(btn => btn.classList.remove('active'));
+
+    button.classList.add('active');
+
+    for (const sectionKey in sections) {
+      if (sectionKey === targetSection) {
+        sections[sectionKey].classList.remove('hidden');
+      } else {
+        sections[sectionKey].classList.add('hidden');
+      }
+    }
+
+
+  });
 });
